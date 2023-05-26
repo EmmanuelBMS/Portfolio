725dffabfe83ee7468ed6b84a207d6b1123b043e
EmmanuelBMS <ebms.1712@gmail.com>
2023-05-26 11:37:40 -0300

    intro criada dentro de sections, removendo html da pagina home

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,41 +1,13 @@
 import React from 'react';
 import Header from '../Components/Header';
-import '../Styles/Home.css';
-import TEXTURE from '../Assets/TEXTURE.jpg';
 import LinksMenu from '../Components/LinksMenu';
+import Intro from '../Sections/Intro';
 
 function Home() {
   return (
     <div>
       <Header />
-      <section>
-        <div className="main-content-div">
-          <img
-            className="main-content-background"
-            src={TEXTURE}
-            alt="Textura do fundo do site"
-          />
-          <div className="main-content-div-tittles">
-            <h1>EMMANUEL BRUNNO</h1>
-            <h3>
-              DESENVOLVEDOR WEB FULL STACK
-            </h3>
-            <p>
-              Crio sites e aplicativos modernos.
-            </p>
-          </div>
-          <div className="main-button-div">
-            <button type="button">
-              DOWNLOAD CV
-            </button>
-          </div>
-        </div>
-        <div className="main-scrollicon-div">
-          <div className="scrollicon-border">
-            <div className="scrollicon-animation" />
-          </div>
-        </div>
-      </section>
+      <Intro />
       <LinksMenu />
     </div>
   );
---
+++ src/Sections/Intro.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import TEXTURE from '../Assets/TEXTURE.jpg';
+import '../Styles/Intro.css';
+
+function Intro() {
+  return (
+    <section>
+      <div className="main-content-div">
+        <img
+          className="main-content-background"
+          src={TEXTURE}
+          alt="Textura do fundo do site"
+        />
+        <div className="main-content-div-tittles">
+          <h1>EMMANUEL BRUNNO</h1>
+          <h3>
+            DESENVOLVEDOR WEB FULL STACK
+          </h3>
+          <p>
+            Crio sites e aplicativos modernos.
+          </p>
+        </div>
+        <div className="main-button-div">
+          <button type="button">
+            DOWNLOAD CV
+          </button>
+        </div>
+      </div>
+      <div className="main-scrollicon-div">
+        <div className="scrollicon-border">
+          <div className="scrollicon-animation" />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Intro;
